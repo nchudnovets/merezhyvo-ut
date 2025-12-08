b988c20a7475fc1269cf52f03c3943d38ebb03a3
Nazar Chudnovets <nazar.chudnovets@avid.com>
2025-12-08 16:47:21 +0200

    styles

--- src/components/toolbar/NavButtons.tsx
+++ src/components/toolbar/NavButtons.tsx
@@ -100,24 +100,13 @@ const NavButtons: React.FC<NavButtonsProps> = ({
         >
           <svg
             viewBox="0 0 16 16"
-            style={{ ...toolbarStyles.navIcon, ...(modeStyles.toolbarBtnIcn ?? {}) }}
+            style={{ ...toolbarStyles.navIcon, ...(modeStyles.toolbarBtnIcn ?? {}), ...{ width: '18px', height: '18px' } }}
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="1.5"
-              d="M12.5 5.5A4.5 4.5 0 1 0 13 9.5"
-            />
-            <path
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="1.5"
-              d="M12.5 5.5H9.5M12.5 5.5V8.5"
+              d="M234.666667,149.333333 L234.666667,106.666667 L314.564847,106.664112 C287.579138,67.9778918 242.745446,42.6666667 192,42.6666667 C109.525477,42.6666667 42.6666667,109.525477 42.6666667,192 C42.6666667,274.474523 109.525477,341.333333 192,341.333333 C268.201293,341.333333 331.072074,284.258623 340.195444,210.526102 L382.537159,215.817985 C370.807686,310.617565 289.973536,384 192,384 C85.961328,384 0,298.038672 0,192 C0,85.961328 85.961328,0 192,0 C252.316171,0 306.136355,27.8126321 341.335366,71.3127128 L341.333333,0 L384,0 L384,149.333333 L234.666667,149.333333 Z"
+              fill="currentColor"
+              transform="scale(0.0416667)"
             />
           </svg>
         </button>
