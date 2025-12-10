88789b178dcca97189d68797d733794053bacd7d
Nazar Chudnovets <nazar.chudnovets@avid.com>
2025-12-10 20:59:05 +0200

    styles

--- src/pages/siteData/SiteDataPage.tsx
+++ src/pages/siteData/SiteDataPage.tsx
@@ -486,8 +486,8 @@ const SiteDataPage: React.FC<ServicePageProps> = ({ mode, serviceUrl }) => {
               >
                 <svg
                   viewBox="0 0 24 24"
-                  width={mode === 'mobile' ? 26 : 16}
-                  height={mode === 'mobile' ? 26 : 16}
+                  width={mode === 'mobile' ? 50 : 16}
+                  height={mode === 'mobile' ? 50 : 16}
                   stroke="currentColor"
                   strokeWidth="2"
                   fill="none"
